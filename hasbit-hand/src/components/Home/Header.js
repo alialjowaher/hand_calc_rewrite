0bf80c05c8ec304469de4a76fa8a727e10f18a97
Ali Jowaher <alinet010@gmail.com>
2021-11-21 21:56:49 +0300

    added all

---
+++ hasbit-hand/src/components/Home/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+const Header = () => {
+    return (
+        <div className="bg-primary text-white p-2">
+            <div className="row">
+             <div className="col-2">
+            <div className="btn btn-primary  text-white rounded pl-2 pr-2 border">Reset</div>
+                 </div>
+                 <div className="col">
+                     
+            <h2 className="text-left ps-5">حاسبة الهاند</h2>
+                     </div>   
+            </div>
+
+        </div>
+    )
+
+
+}
+
+export default Header
