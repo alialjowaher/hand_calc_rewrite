1e12a2e5c3abe553c65cfad358efc4a48c85099a
Ali Jowaher <alinet010@gmail.com>
2021-11-27 18:50:38 +0300

    clear form after submition, added reducer for testing

--- hasbit-hand/src/components/AddJawlah.js
+++ hasbit-hand/src/components/AddJawlah.js
@@ -137,7 +137,7 @@ const AddJawlah = (props) => {
 
   }
 
-   const handleSubmit = (evt) => {
+  const handleSubmit = (evt) => {
     const winValues = [
       { winType: 'khlosSafi', winner_team: '', losser_team: '', winner_value: -30, losser_value: 300, nazil_count: 0, nazil_total: 0, isTasjilah: 'false' },
       { winType: 'dabalSafi', winner_team: '', losser_team: '', winner_value: -60, losser_value: 600, nazil_count: 0, nazil_total: 0, isTasjilah: 'false' },
@@ -182,29 +182,43 @@ const AddJawlah = (props) => {
           break;
       }
     };
-    
-    
+
+
     async function saveJwalah() {
       try {
-          await db.jawlatStore.add({
-            winType : finalWinType,
-            winner_team: jawlahResult[0].winner_team,
-            losser_Team: jawlahResult[0].losser_team,
-            winner_value: jawlahResult[0].winner_value,
-            losser_value: jawlahResult[0].losser_value,
-            nazil_count: jawlahResult[0].nazil_count,
-            nazil_total: jawlahResult[0].nazil_total,
-            isTasjilah: jawlahResult[0].isTasjilah
+        await db.jawlatStore.add({
+          winType: finalWinType,
+          winner_team: jawlahResult[0].winner_team,
+          losser_Team: jawlahResult[0].losser_team,
+          winner_value: jawlahResult[0].winner_value,
+          losser_value: jawlahResult[0].losser_value,
+          nazil_count: jawlahResult[0].nazil_count,
+          nazil_total: jawlahResult[0].nazil_total,
+          isTasjilah: jawlahResult[0].isTasjilah
 
-          })
-      }catch (error) {
+        })
+      } catch (error) {
         console.log('failed to add');
       }
     }
 
+    function resetInputs() {
+
+      setWinnerTeamVal('');
+      setwinTypeVal('');
+      setsubWinTypeVal('');
+      setnazilCountVal(0);
+      setnazilTotalVal(0);
+      settasjilahTypeVal('');
+
+    };
+
+
+    evt.preventDefault();
     saveJwalah();
-    // evt.preventDefault();
+    resetInputs();
     props.onHide();
+
     // console.log(jawlahResult);
   }
 
--- hasbit-hand/src/components/Content.js
+++ hasbit-hand/src/components/Content.js
@@ -1,5 +1,5 @@
-import {React, useState} from 'react'
-import {ProgressBar,Container,Button} from 'react-bootstrap'
+import { React, useState } from 'react'
+import { ProgressBar, Container, Button } from 'react-bootstrap'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
@@ -13,71 +13,70 @@ import { useLiveQuery } from 'dexie-react-hooks';
 
 
 const Content = () => {
-    const progresBarVal = (2/7 * 100)
+    const progresBarVal = (2 / 7 * 100)
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    
-    const jawlats = useLiveQuery(
-        async() => {
-            const jawlats = await db.jawlatStore.toArray()
-            return (jawlats)
-        },
-        )    
-        
-    
-    //lanaTotal 
+
+    const jawlats = useLiveQuery(() => db.jawlatStore.toArray(function (result) {
+        const count = result.reduce((counter, { winType }) =>
+            winType === "dabalSafi" ? (counter += 1) : counter, 0);
+        return count;
+    }));
+
+
+    let lanaTotal = jawlats;
     //lahomTotal
     //farq
+    let allJawlatCount = useLiveQuery(() => db.jawlatStore.count());
     let jawaltCount = useLiveQuery(() => db.jawlatStore.where('isTasjilah').equals('false').count());
-    let allJawlatCount =  useLiveQuery(() => db.jawlatStore.count());
-  
+
     return (
         <Container fluid className="overflow-hidden">
-              <AddJawlah
-       show={show}
-       onHide={handleClose}
-       backdrop=""
-       keyboard={false}
-       />
+            <AddJawlah
+                show={show}
+                onHide={handleClose}
+                backdrop=""
+                keyboard={false}
+            />
             <div className="row p-4 card shadow m-3 rounded">
-            <div className="col-12 ">
-                <div className="row d-flex justify-content-between text-center mb-3">
-                    
-                    <div className="col-4 h3">لـهم</div>
-                    <div className="col-4 text-danger h3 font-weight-bolder">الفرق</div>
-                    <div className="col-4 h3">لـنا</div>
-                    
+                <div className="col-12 ">
+                    <div className="row d-flex justify-content-between text-center mb-3">
+
+                        <div className="col-4 h3">لـهم</div>
+                        <div className="col-4 text-danger h3 font-weight-bolder">الفرق</div>
+                        <div className="col-4 h3">لـنا</div>
+
+                    </div>
+                    <div className="row d-flex justify-content-between text-center">
+                        <div className="col-4 h4">360</div>
+                        <div className="col-4 h4 text-danger">1200</div>
+                        <div className="col-4 h4">{lanaTotal}</div>
+                    </div>
                 </div>
-                <div className="row d-flex justify-content-between text-center">
-                    <div className="col-4 h4">360</div>
-                    <div className="col-4 h4 text-danger">1200</div>
-                    <div className="col-4 h4">720</div>
-                </div>
-                </div>         
             </div>
             <div className="row p-3 card shadow p-4 m-3 rounded">
-            <div className="col-12">
-                <div className="row d-flex justify-content-between text-center mb-3">
-                    <div className="col h4">مجموع الجولات</div>
-                    <div className="col h4 ">الجولة الحالية</div>
+                <div className="col-12">
+                    <div className="row d-flex justify-content-between text-center mb-3">
+                        <div className="col h4">مجموع الجولات</div>
+                        <div className="col h4 ">الجولة الحالية</div>
+                    </div>
+                    <div className="row d-flex justify-content-between text-center">
+                        <div className="col h4">{allJawlatCount}</div>
+                        <div className="col h4">{jawaltCount}</div>
+                    </div>
                 </div>
-                <div className="row d-flex justify-content-between text-center">
-                    <div className="col h4">{allJawlatCount}</div>
-                    <div className="col h4">{jawaltCount}</div>
-                </div>
-                </div>  
                 <div className="row m-0 pt-3 p-0">
-                <ProgressBar className="m-0 p-0" dir="RTL" now={progresBarVal} />
+                    <ProgressBar className="m-0 p-0" dir="RTL" now={progresBarVal} />
                 </div>
             </div>
-          
+
             <div className="bg-white text-white rounded-top fixed-bottom icon-bar">
                 <div className="row text-center justify-content-between pt-2 pb-2">
-                <div className="col btn btn-white"><FontAwesomeIcon icon={faList} /></div>
-                <div className="col-2 btn btn-primary rounded" onClick={handleShow}><FontAwesomeIcon icon={faPlus} /></div>
-                <div className="col btn btn-white"><FontAwesomeIcon icon={faHome} /></div>
+                    <div className="col btn btn-white"><FontAwesomeIcon icon={faList} /></div>
+                    <div className="col-2 btn btn-primary rounded" onClick={handleShow}><FontAwesomeIcon icon={faPlus} /></div>
+                    <div className="col btn btn-white"><FontAwesomeIcon icon={faHome} /></div>
                 </div>
             </div>
 
